a58a1691368255a320536d7ba9af091226ab159d
khoirotulfadhilah <36910775+khoirotulfadhilah@users.noreply.github.com>
2018-12-18 15:47:48 +0700

    Add files via upload

---
+++ Praktikum9/Mahasiswa.java
@@ -0,0 +1,30 @@
+package Praktikum9;
+
+public class Mahasiswa{
+
+    int nis;
+    String nama_mhs;
+
+    public Mahasiswa(int n){
+
+    }
+
+    public void setNim(int n){
+        
+    }
+
+    public int getNim(){
+        
+        return nis;
+    }
+
+    public void setNama(String na){
+        
+    }
+
+    public String getNama(int n){
+
+        return nama_mhs;
+        
+    }
+}
---
+++ Praktikum9/RumusMatematika.java
@@ -0,0 +1,19 @@
+package Praktikum9;
+
+public class RumusMatematika{
+
+    double a, t;
+    double l_segitiga;
+
+    public static void main(String args[]){
+        RumusMatematika rm = new RumusMatematika();
+        rm.luasSegitiga();
+    }
+
+    void luasSegitiga(){
+        a = 7;
+        t = 10;
+        l_segitiga = 0.5 * 1 * t;
+        System.out.println("Luas Segitiga = "+l_segitiga);
+    }
+}
